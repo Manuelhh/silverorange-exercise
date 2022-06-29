03ea40da73b8dc882a5498299940038d93a4d2b9
Manuel Hinojosa <mangud.hinojosa@gmail.com>
2022-06-29 17:40:09 -0400

    6th commit: added event listener to language button / created function that filters all repos according to the language / added details window / added functionality to the details window to show and show off /

--- web/src/App.js
+++ web/src/App.js
@@ -1,11 +1,12 @@
 import s from './App.module.css';
-// import { Routes, Route } from 'react-router-dom';
 import { useEffect, useState } from 'react';
 import axios from 'axios';
 
 export function App() {
   // initiliazing data
   const [allRepos, setAllRepos] = useState([]);
+  // initial data for displaying repo details div
+  const [displayDetail, setDisplayDetail] = useState(false);
 
   // fetching data upon loading and setting it to app state
   useEffect(() => {
@@ -32,13 +33,34 @@ export function App() {
       {/* list of repos */}
       {orderedRepos.map((repo) => (
         <div className={s.repoContainer} key={repo.id}>
-          <div>Name: {repo.name} </div>
+          <div>
+            <br />
+            <button onClick={() => setDisplayDetail(!displayDetail)}>
+              Name: {repo.name}
+            </button>
+          </div>
           <div>Desc: {repo.description ? repo.description : 'n/a'} </div>
           <div>
             language:
             <button onClick={(e) => filterRepos(e)}>{repo.language}</button>
           </div>
           <div>Forks Count: {repo.forks} </div>
+
+          {displayDetail ? (
+            <div>
+              <div> +++details+++ </div>
+              <div> most recent commit date: </div>
+              <div> author: </div>
+              <div> message: </div>
+              <div> README: </div>
+
+              <button onClick={() => setDisplayDetail(false)}>
+                close details
+              </button>
+            </div>
+          ) : (
+            <div>{''}</div>
+          )}
         </div>
       ))}
     </div>

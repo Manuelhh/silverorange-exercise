daac0e4f0f856bb84f7acd5bef9cd80b4339ca1f
Manuel Hinojosa <mangud.hinojosa@gmail.com>
2022-06-29 10:27:49 -0400

    3rd commit: Imported axios into repos function file / imported json repos data into repos function file / fetched gitHub repos data using axios/ added gitHub & json repos data into a single array / filtered and returned only repos with property fork set to false.

--- api/src/routes/repos.ts
+++ api/src/routes/repos.ts
@@ -1,14 +1,36 @@
 import { Router, Request, Response } from 'express';
 
+// import axios for fetching data
+import axios from 'axios';
+// import json data from repos.json
+import jsonRepos from '../../../api/data/repos.json';
+import { json } from 'stream/consumers';
+
 export const repos = Router();
 
-repos.get('/', async (_: Request, res: Response) => {
+repos.get('/', async (req, res) => {
   res.header('Cache-Control', 'no-store');
-
   res.status(200);
-
   // TODO: See README.md Task (A). Return repo data here. You’ve got this!
 
-  
-  res.json([]);
+  //
+  // fetching data from github's api
+  // all gitHub repos
+  const gitHubRepos = await axios
+    .get('https://api.github.com/users/silverorange/repos')
+    .then((res) => {
+      return res.data;
+    })
+    .catch((err) => {
+      console.log('something happened');
+      console.log(err);
+    });
+  // adding array of json & github repos together
+  const allRepos = gitHubRepos.concat(jsonRepos);
+  // filtering repo.fork false from all repos
+  const forkFalseRepos = allRepos.filter((repository: any) => {
+    return repository.fork === false;
+  });
+  // returning repo.fork false repos
+  res.json(forkFalseRepos);
 });

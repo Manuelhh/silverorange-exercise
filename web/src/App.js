ba7b9e58803f3f06367dd4c4a9e377a565ae167b
Manuel Hinojosa <mangud.hinojosa@gmail.com>
2022-06-29 16:29:53 -0400

    6th commit: added event listener to language button, created function that handles the event and filters the list of repos according to the language / installed and imported react-router

--- web/src/App.js
+++ web/src/App.js
@@ -1,29 +1,42 @@
 import s from './App.module.css';
+// import { Routes, Route } from 'react-router-dom';
 import { useEffect, useState } from 'react';
 import axios from 'axios';
 
 export function App() {
+  // initiliazing data
   const [allRepos, setAllRepos] = useState([]);
 
+  // fetching data upon loading and setting it to app state
   useEffect(() => {
     axios.get('http://localhost:4000/repos').then((res) => {
       setAllRepos(res.data);
     });
-  }, [allRepos]);
+  }, []);
 
+  // ordering data as requested
   const orderedRepos = allRepos.slice().sort((a, b) => {
     return Date.parse(a.created_at) - Date.parse(b.created_at);
   });
 
+  // filters repos according to language
+  const filterRepos = (e) => {
+    console.log(e.target.innerHTML);
+    setAllRepos(
+      allRepos.filter((repo) => repo.language === e.target.innerHTML)
+    );
+  };
+
   return (
     <div className={s.appContainer}>
+      {/* list of repos */}
       {orderedRepos.map((repo) => (
         <div className={s.repoContainer} key={repo.id}>
           <div>Name: {repo.name} </div>
           <div>Desc: {repo.description ? repo.description : 'n/a'} </div>
           <div>
             language:
-            <button>{repo.language}</button>
+            <button onClick={(e) => filterRepos(e)}>{repo.language}</button>
           </div>
           <div>Forks Count: {repo.forks} </div>
         </div>
